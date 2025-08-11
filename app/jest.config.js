462e300333961aa8192b70ac0945e58004d9a7d0
Dheeraj Gundavaram <dheeraj.gundavaram@wipro.com>
2025-08-11 11:11:17 +0000

    Add jest-junit reporter

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js']
+  testMatch: ['**/__tests__/**/*.test.js', '**/tests/**/*.test.js'],
+  reporters: ['default', ['jest-junit', { outputDirectory: '.', outputName: 'junit.xml' }]]
 };
-
