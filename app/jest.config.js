603c0ba9b8a5b280ae5ec2e4c66e12b777771b1a
Dheeraj Gundavaram <dheeraj.gundavaram@wipro.com>
2025-08-11 12:07:39 +0000

    Add CI and Agentic-Heal workflows + reporters

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js', '**/tests/**/*.test.js'],
-  reporters: ['default', ['jest-junit', { outputDirectory: '.', outputName: 'junit.xml' }]]
+	  testEnvironment: 'node',
+	  testMatch: ['**/__tests__/**/*.test.js', '**/tests/**/*.test.js'],
+	  reporters: ['default', ['jest-junit', { outputDirectory: '.', outputName: 'junit.xml' }]]
 };
+
